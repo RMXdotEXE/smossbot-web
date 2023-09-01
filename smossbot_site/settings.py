2f7400d7b5a05a4ddd8649c12060c2a50d3edfda
Omar Gonzalez <omarg19999@gmail.com>
2023-09-01 12:21:31 +0000

    ensure server settings

--- smossbot_site/settings.py
+++ smossbot_site/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 
 hostarray = []
@@ -93,7 +93,7 @@ DATABASES = {
         'USER': 'moz',
         'PASSWORD': '***REMOVED***',
         'HOST': 'localhost',
-        'PORT': '',
+        'PORT': '5433',
     }
 }
 
